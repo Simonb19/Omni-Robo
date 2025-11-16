0890e1fa4fd1a2a03f09be3880f8ae035bb5c327
Simon <simon.bachleitner@outlook.com>
2025-11-16 18:59:01 +0100

    Change joystick library

--- src/components/joystick-1d.tsx
+++ src/components/joystick-1d.tsx
@@ -1,23 +1,43 @@
 import type { Joystick1DProps } from "../types/component-types";
-import Joystick, { Direction, type IJoystickChangeValue } from 'rc-joystick';
+import { Joystick, JoystickShape } from "react-joystick-component";
+import type { IJoystickUpdateEvent } from "react-joystick-component/build/lib/Joystick";
 
-function Joystick1D ({ onChange, lockDirection = 'x', baseRadius = 75, ...otherProps}:Joystick1DProps) {
+function Joystick1D ({ onChange, lockDirection = 'x', baseRadius = 75, ...otherProps}: Joystick1DProps) {
+    
+    const handleMove = (event: IJoystickUpdateEvent) => {
+        if (!event.y && !event.x) return;
+        
+        console.log(`Y: ${event.y} X: ${event.x}`);
+
+        // Für Y-Achse (vertikal)
+        if (lockDirection === 'y') {
+            const normalized = Math.round(-(event.y || 0)  * 100);
+            onChange(normalized);
+        } 
+        // Für X-Achse (horizontal)
+        else {
+            const normalized = Math.round((event.x || 0) * 100);
+            onChange(normalized);
+        }
+    };
+
+    const handleStop = () => {
+        onChange(0);
+    };
 
     return (
-        <Joystick 
-        lockX={lockDirection === 'x'}
-        lockY={lockDirection === 'y'}
-        throttle={50}
-        onChange={(val: IJoystickChangeValue) => {
-            const normalized = val.direction === Direction.Top 
-            ? val.distance * 100 / baseRadius
-            : -(val.distance * 100 / baseRadius) || 0;
-
-            onChange(normalized);
-        }}
-        {...otherProps}
+        <Joystick
+            size={baseRadius * 2}
+            baseColor="#bdbdbdff"
+            stickColor="#ffffffff"
+            move={handleMove}
+            stop={handleStop}
+            controlPlaneShape={JoystickShape.AxisX}
+            throttle={50}
+            minDistance={15}
+            {...otherProps}
         />
-    )
+    );
 }
 
-export default Joystick1D
+export default Joystick1D;
--- src/types/component-types.ts
+++ src/types/component-types.ts
@@ -1,6 +1,7 @@
-import type { IJoystickProps } from "rc-joystick"
+import type { IJoystickProps } from "react-joystick-component/build/lib/Joystick";
 
 export type Joystick1DProps = {
     onChange: (val: number) => void,
     lockDirection?: 'y' | 'x',
+    baseRadius?: number,
 } & Omit<IJoystickProps, 'onChange'>
