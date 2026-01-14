2f3c5125d47f708f16bfb108e717d40997958b4c
Simon <simon.bachleitner@outlook.com>
2026-01-14 16:25:44 +0100

    bug fix

--- src/app.tsx
+++ src/app.tsx
@@ -6,8 +6,7 @@ import { useConnection } from './hooks/use-connection';
 function App() {
   const { isConnected } = useConnection();
 
-  // return isConnected ? <ControlScreen /> : <ConnectionScreen />;
-  return <ControlScreen />;
+  return isConnected ? <ControlScreen /> : <ConnectionScreen />;
 }
 
 export default App;
